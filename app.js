d82e3793bac45e3f2bcd105f98f094c7637a37fd
Britt Carlton <brittanyrc95@gmail.com>
2022-07-12 11:34:21 -0400

    Add inquirer package for prompting user for input

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // node modules
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-// global variables
-const profileDataArgs = process.argv.slice(2);
-const [user, github] = profileDataArgs;
+// // global variables
+// const pageHTML = generatePage(user, github);
 
-// function calls
-fs.writeFile("./index.html", generatePage(user, github), err => {
-    if (err) throw err;
+// // function calls
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if (err) throw err;
 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+// questions via inquirer
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
